1a73c0f6f3ee2461a52f215193b242decb8fa8fb
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2023-08-28 18:05:59 +0200

    Update

--- content/_data/images.js
+++ content/_data/images.js
@@ -1,11 +1,8 @@
-const path = require("path");
 const fetchGalleriesFromApi = require("./galleries");
-const siteConfig = require("../_data/siteConfig");
 
 const fetchImagesFromApi = async () => {
   try {
     const galleries = await fetchGalleriesFromApi();
-
     const galleryCovers = galleries.map(({ cover }) => cover);
 
     const images = galleries.flatMap(({ images }) =>
