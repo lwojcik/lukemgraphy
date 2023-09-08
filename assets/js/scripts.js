429a83dbfaf8eed16e4369f37f47521fca3b05e8
Łukasz Wójcik <1711174+lwojcik@users.noreply.github.com>
2023-09-08 02:39:23 +0200

    Update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -74,63 +74,23 @@ const detectLocalStorageDarkMode = () => {
   }
 };
 
+const handleMobileMenu = () => {
+  const menuBtn = document.querySelector(".menu-btn");
+  const menuBtnCheckbox = document.querySelector(".menu-checkbox");
+
+  menuBtn.addEventListener("keyup", (e) => {
+    e.preventDefault();
+    if (e.key === "Enter") {
+      const { checked } = menuBtnCheckbox;
+      menuBtnCheckbox.checked = !checked;
+    }
+  });
+};
+
 window.addEventListener("DOMContentLoaded", () => {
   enableJS();
   detectSystemWideDarkMode();
   detectLocalStorageDarkMode();
   themeSwitcherButton();
+  handleMobileMenu();
 });
-
-//   // Menu
-
-//   var menu = document.getElementById("menu"),
-//     rollback,
-//     WINDOW_CHANGE_EVENT =
-//       "onorientationchange" in window ? "orientationchange" : "resize";
-
-//   function toggleHorizontal() {
-//     menu.classList.remove("closing");
-//     [].forEach.call(
-//       document.getElementById("menu").querySelectorAll(".custom-can-transform"),
-//       function (el) {
-//         el.classList.toggle("pure-menu-horizontal");
-//       }
-//     );
-//   }
-
-//   function toggleMenu() {
-//     if (menu.classList.contains("open")) {
-//       menu.classList.add("closing");
-//       rollBack = setTimeout(toggleHorizontal, 500);
-//     } else {
-//       if (menu.classList.contains("closing")) {
-//         clearTimeout(rollBack);
-//       } else {
-//         toggleHorizontal();
-//       }
-//     }
-//     menu.classList.toggle("open");
-//     document.getElementById("toggle").classList.toggle("x");
-//   }
-
-//   function closeMenu() {
-//     if (menu.classList.contains("open")) {
-//       toggleMenu();
-//     }
-//   }
-
-//   document.getElementById("toggle").addEventListener("click", function (e) {
-//     toggleMenu();
-//     e.preventDefault();
-//   });
-
-//   document.getElementById("toggle").addEventListener("keyup", function (e) {
-//     e.preventDefault();
-//     if (e.key === "Enter") {
-//       toggleMenu();
-//       e.preventDefault();
-//     }
-//   });
-
-//   window.addEventListener(WINDOW_CHANGE_EVENT, closeMenu);
-// });
